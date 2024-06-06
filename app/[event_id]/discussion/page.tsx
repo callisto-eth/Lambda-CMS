22a3aee8211a4b84f8c5582fbb4174bbe5801935
0xKits <kitsgit@gmail.com>
2024-06-06 11:37:28 +0530

    feat: Messaging partially implemented

--- app/[event_id]/discussion/page.tsx
+++ app/[event_id]/discussion/page.tsx
@@ -12,34 +12,103 @@ import {
   FormLabel,
 } from '@/components/ui/form';
 import { z } from 'zod';
-import { useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import MessageContent from '@/components/MessageContent';
+import { createClient } from '@/utils/supabase/client';
+import { useState } from 'react';
+
+type Message = {
+  id: number;
+  author: string;
+  body: string;
+  chat: string;
+  timestamp: string;
+};
 
 export default function Chat({ params }: { params: { event_id: string } }) {
+
+  const chat = "a7e5cc4b-f9f8-4624-b86f-335f90458e73"
+  const supabase = createClient();
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  async function streamMessages(chat: string,) {
+    supabase
+      .channel('schema-db-changes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'connections',
+          table: 'chat_messages',
+          filter: `chat=eq.${chat}`
+        },
+        (payload) => {
+          console.log(payload.new)
+          setMessages((prev) => [
+            ...prev,
+            {
+              id: payload.new.id,
+              author: payload.new.author,
+              body: payload.new.content,
+              chat: payload.new.chat,
+              timestamp: payload.new.created_at,
+            },
+          ]);
+        },
+      )
+      .subscribe();
+  }
+
+  streamMessages(chat);
+
+
   return (
     <main className="relative lg:mx-10 mx-5 *:font-DM-Sans flex flex-col lg:h-[calc(100vh-150px)] h-[calc(100vh-110px)]">
       <ScrollArea className="flex-grow">
-        <MessageContent />
-        <MessageContent />
+        {
+          messages.map((m) => {
+            return (
+              <MessageContent
+                key={m.id}
+                id={m.id}
+                author={m.author}
+                body={m.body}
+                timestamp={m.timestamp}
+              />
+            );
+
+          })
+        }
       </ScrollArea>
-      <MessageBox />
+      <MessageBox chat={chat} />
     </main>
   );
 }
 
-const MessageBox = () => {
-  const sendMessageSchema = z.object({});
+const MessageBox = ({ chat }: { chat: string }) => {
+  const sendMessageSchema = z.object({
+    message: z.string().min(1),
+  });
   const sendMessageForm = useForm({
     resolver: zodResolver(sendMessageSchema),
   });
 
-  function onSubmit(fieldValues: z.infer<typeof sendMessageSchema>) {}
+  function onSubmit(fieldValues: z.infer<typeof sendMessageSchema>) {
+    fetch('/api/chat/message/send', {
+      method: 'POST',
+      body: JSON.stringify({
+        chat: chat,
+        body: fieldValues.message
+      }),
+    });
+  }
   return (
     <Form {...sendMessageForm}>
       <form
-        onSubmit={sendMessageForm.handleSubmit(onSubmit)}
+        // FIXME: wtf is this shit?
+        onSubmit={sendMessageForm.handleSubmit(onSubmit as SubmitHandler<FieldValues>)}
         className="w-full mt-5 border border-opacity-10 border-[#948B96] bg-background rounded-xl"
       >
         <FormField
@@ -55,6 +124,7 @@ const MessageBox = () => {
                     id="message"
                     placeholder="Type your message here..."
                     className="resize-none border-0 text-base p-3 px-6 shadow-none focus-visible:ring-0 focus:outline-none focus:ring-0 "
+                    {...field}
                   />
                   <Button
                     type="submit"
--- app/api/chat/message/send/route.ts
+++ app/api/chat/message/send/route.ts
@@ -2,11 +2,33 @@ import { createClient } from '@/utils/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 type SendMessageSchema = {
-  event: string;
+  chat: string;
   body: string;
 };
 
 export async function POST(req: NextRequest) {
+  const data: SendMessageSchema = await req.json();
+  console.log(data)
   const supabase = createClient();
   const user = await supabase.auth.getUser();
+  let { data: sendMessageResponse, error: sendMessageError } = await supabase
+    .schema('connections')
+    .from('chat_messages')
+    //@ts-ignore - fuck you typescript retarded bitch
+    .insert({
+      chat: data.chat,
+      author: user.data.user?.id,
+      content: data.body,
+    })
+    .select();
+
+  if (sendMessageError) {
+    console.log(sendMessageError.message)
+    return NextResponse.json(
+      { error: sendMessageError.message },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({ data: sendMessageResponse }, { status: 200 });
 }
