b57da0df269feea358a33ea10d2af22d20576a68
Vishal <vismbs@pm.me>
2024-06-07 21:23:25 +0530

    fix: key_id

--- app/api/payment/invoice/create/route.ts
+++ app/api/payment/invoice/create/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_ID!,
+  key_id: process.env.RAZORPAY_ID as string,
   key_secret: process.env.RAZORPAY_SECRET,
 });
 
