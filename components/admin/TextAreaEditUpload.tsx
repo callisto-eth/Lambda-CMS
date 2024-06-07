73383ffa64619fc2dcda32302de01badee405f66
Vishal <vismbs@pm.me>
2024-06-07 12:42:28 +0530

    fix: added displayName for components

--- components/admin/TextAreaEditUpload.tsx
+++ components/admin/TextAreaEditUpload.tsx
@@ -62,3 +62,5 @@ export const TextAreaEditUpload = React.forwardRef<
     </p>
   );
 });
+
+TextAreaEditUpload.displayName = 'TextAreaEditUpload';
--- components/admin/TextEditUpload.tsx
+++ components/admin/TextEditUpload.tsx
@@ -49,3 +49,5 @@ export const TextEditUpload = React.forwardRef<
     </p>
   );
 });
+
+TextEditUpload.displayName = 'TextEditUpload';
