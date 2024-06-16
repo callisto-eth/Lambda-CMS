85b814af4b6d96699ef32dc48f6113d7f2d5d8eb
Vishal <vismbs@pm.me>
2024-06-16 11:21:44 +0530

    refactor: removed error handlers

--- app/[event_id]/page.tsx
+++ app/[event_id]/page.tsx
@@ -34,7 +34,7 @@ export default async function EventInfo({
     await fetch(
       process.env.NODE_ENV === 'production'
         ? 'https://lambda.events/api/event/fetch'
-        : `http://localhost:3000/api/event/fetch`,
+        : `https://lambda.events/api/event/fetch`,
       {
         method: 'POST',
         headers: {
@@ -45,8 +45,8 @@ export default async function EventInfo({
     )
   ).json();
 
-  if (fetchedEventResponseError)
-    handleErrors(fetchedEventResponseError, fetchedEventStatus);
+  // if (fetchedEventResponseError)
+  //   handleErrors(fetchedEventResponseError, fetchedEventStatus);
 
   const userData = await supabase.auth.getUser();
 
@@ -73,8 +73,8 @@ export default async function EventInfo({
     )
   ).json();
 
-  if (eventAttendeesError)
-    handleErrors(eventAttendeesError, eventAttendeesStatus);
+  // if (eventAttendeesError)
+  //   handleErrors(eventAttendeesError, eventAttendeesStatus);
 
   if (fetchEventResponseData && eventAttendeesResponse) {
     return (
